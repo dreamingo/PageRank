088dc1360224c85392830bac30928460f140785c
victor <victor@victor-PowerEdge-T110-II>
2012-08-07 14:57:15 -0700

    8.7 2:57

--- PageRank/src/cs/ucla/edu/pagerank/AdjacencyNode.java
+++ PageRank/src/cs/ucla/edu/pagerank/AdjacencyNode.java
@@ -1,42 +1,70 @@
 package cs.ucla.edu.pagerank;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 
 public class AdjacencyNode {
 
-	private Integer flag;
+	private Boolean flag;
 	private Double p;
 	
 	private Long nodeId;
 	private Double rankValue;
-	private ArrayList<Integereger> list;
+	private ArrayList<Long> list;
 	//private PagerankObject pagerankObject;
 	
-	public AdjacencyNode(Double p) {
+	private AdjacencyNode(Double p) {
 		this.p = p;
 		this.flag = false;
 	}
 	
-	public AdjacencyNode(Integer nodeId, Double rankValue) {
+	private AdjacencyNode(Long nodeId, Double rankValue) {
 		this.nodeId = nodeId;
-		this.list = new ArrayList<Integereger>();
+		this.list = new ArrayList<Long>();
 		this.rankValue = rankValue;
 		this.flag = true;
+	}
+	
+	public static AdjacencyNode construct(LongWritable key, Text value) throws Exception {
+		String[] array = value.toString().split("\t");
+		if (array == null || array.length == 0) throw new Exception();
+		
+		Double rankValue = Double.parseDouble(array[0]);
+		
+		if ((array.length == 2) && (array[1].equals("0"))) {
+			//Format:
+			//0.3	0
+			return new AdjacencyNode(rankValue);
+		}
+		else {
+			//Format:
+			//0.3	1	2	3
+			//0.2
+			AdjacencyNode node = new AdjacencyNode(key.get(), rankValue);
+			for (int i=1; i < array.length; i++) {
+				node.addNeighbor(Long.parseLong(array[i]));
+			}
+			return node;
+		}
+		
 	}
 	
 	public Double getP() {
 		return p;
 	}
 	
-	public void addNeighbor(Integer e) {
+	public void addNeighbor(Long e) {
 		list.add(e);
 	}
 	
-	public ArrayList<Integereger> getAdjacencyList() {
+	public ArrayList<Long> getAdjacencyList() {
 		return list;
 	}
 	
-	public Integer getNodeId() {
+	public Long getNodeId() {
 		return nodeId;
 	}
 	
@@ -49,7 +77,29 @@ public class AdjacencyNode {
 	}
 
 	public String toString() {
-		return nodeId + ":" + list.toString() + " rankValue: " + rankValue;
+		if (!flag) {
+			return rankValue + "\t" + 0;
+		}
+		else {
+			String s = "";
+			Iterator<Long> iter = list.iterator();
+			if (iter.hasNext()) {
+				s += "\t" + iter.next();
+			}
+			return rankValue + s;
+		}
+	}
+	
+	public Boolean getFlag() {
+		return flag;
+	}
+	
+	public static void main(String[] args) {
+		AdjacencyNode node = new AdjacencyNode(100L, 0.2);
+		//node.addNeighbor(2L);
+		//node.addNeighbor(4L);
+		
+		System.out.println(node);
 	}
 
 }
--- PageRank/src/cs/ucla/edu/pagerank/Pagerank.java
+++ PageRank/src/cs/ucla/edu/pagerank/Pagerank.java
@@ -10,39 +10,49 @@ import org.apache.hadoop.util.*;
 
 public class Pagerank {
 	
-	public static class Map extends MapReduceBase implements Mapper<LongWritable, AdjacencyNode, LongWritable, AdjacencyNode> {
-		//private final static IntWritable one = new IntWritable(1);
-		//private Text word = new Text();
-
-		public void map(LongWritable key, AdjacencyNode value, OutputCollector<LongWritable, AdjacencyNode> output, Reporter reporter) throws IOException {
-			Double p = value.getRankValue() / value.getAdjacencyList().size();
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, LongWritable, Text> {
+		public void map(LongWritable key, Text value, OutputCollector<LongWritable, Text> output, Reporter reporter) throws IOException {
+			
+			AdjacencyNode node = null;
+			try {
+				node = AdjacencyNode.construct(key, value);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			Double p = node.getRankValue() / node.getAdjacencyList().size();
 			
 			output.collect(key, value);
-			
 			Iterator<Long> iter = node.getAdjacencyList().iterator();
 			while (iter.hasNext()) {
-				output.collect(iter.next(), new AdjacencyNode(p));
+				output.collect(new LongWritable(iter.next()), new Text(p + "\t" + 0));
 			}
 		}
 	}
 	
-	public static class Reduce extends MapReduceBase implements Reducer<LongWritable, AdjacencyNode, LongWritable, AdjacencyNode> {
-		public void reduce(LongWritable key, Iterator<AdjacencyNode> values, OutputCollector<LongWritable, AdjacencyNode> output, Reporter reporter) throws IOException {
+	public static class Reduce extends MapReduceBase implements Reducer<LongWritable, Text, LongWritable, Text> {
+		public void reduce(LongWritable key, Iterator<Text> values, OutputCollector<LongWritable, Text> output, Reporter reporter) throws IOException {
+			
 			AdjacencyNode node = null;
 			double s = 0;
-			
 			while (values.hasNext()) {
-				AdjacencyNode p = values.next();
-				if (p.getFlag() == true) {
-					node = (AdjacencyNode)p;
+				AdjacencyNode temp = null;
+				try {
+					temp = AdjacencyNode.construct(key, values.next());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				if (node.getFlag()) {
+					node = temp;
 				}
 				else {
-					s += p.getP();
+					s += temp.getP();
 				}
 			}
 			
 			node.setRankValue(s);
-			output.collect(key, node);
+			output.collect(key, new Text(node.toString()));
 		}
 	}
 	
@@ -51,13 +61,13 @@ public class Pagerank {
 		conf.setJobName("pagerank");
 
 		conf.setOutputKeyClass(LongWritable.class);
-		conf.setOutputValueClass(AdjacencyNode.class);
+		conf.setOutputValueClass(Text.class);
 
 		conf.setMapperClass(Map.class);
 		conf.setCombinerClass(Reduce.class);
 		conf.setReducerClass(Reduce.class);
 
-		conf.setInputFormat(TextInputFormat.class);
+		conf.setInputFormat(KeyValueTextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
 
 		FileInputFormat.setInputPaths(conf, new Path(args[0]));

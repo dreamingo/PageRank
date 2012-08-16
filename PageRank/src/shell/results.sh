7f8c885bfcd11d2e47e26e270c57573b1d171977
victor <victor@victor-PowerEdge-T110-II>
2012-08-15 18:39:06 -0700

    c

--- PageRank/src/shell/results.sh
+++ PageRank/src/shell/results.sh
@@ -6,6 +6,6 @@ then
 	exit
 fi
 
-cd ~/hadoop-1.0.3/hadoop-1.0.3/backup/"$1"
+cd ~/hadoop-1.0.3/hadoop-1.0.3/backup/Wiki-Vote_Origin/"$1"
 
-cat part-00000 | awk '{print $1"\t"$2}' | sort -r -n -k 2 | less
+cat part-00000 | awk '{print $1"\t"$2}' | sort -r -g -k 2 | less
